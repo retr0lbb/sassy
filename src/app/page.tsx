d4dd2ba2ffa30cb64b0f7fde82678cc469fdd701
retr0lbb <retr0lbb@gmail.com>
2025-10-04 20:57:34 -0300

    feat: add component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { SideMenu } from "@/components/side-menu";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-dvh bg-zinc-950">
-      <p>Hello word</p>
+    <div className="w-full min-h-dvh bg-slate-950 flex">
+      <SideMenu />
+      <div className="text-amber-50">Hello this is memes</div>
     </div>
   );
 }
